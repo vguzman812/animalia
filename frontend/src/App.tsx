74d69773906d40538a6abc3cc489eb3ff09819d0
vincent <100250184+vguzman812@users.noreply.github.com>
2023-09-13 16:03:45 -0400

    Add front end login functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/Container"
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 import {Outlet} from "react-router-dom"
 
 
@@ -14,6 +16,7 @@ const App = () => {
         </Container>
       </main>
       <Footer/>
+      <ToastContainer/> 
     </>
   )
 }
--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const submitHandler = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const [login, { isLoading }] = useLoginMutation();
+	const { userInfo } = useSelector((state) => state.auth);
+
+	const { search } = useLocation();
+	const sp = new URLSearchParams(search);
+	const redirect = sp.get("redirect") || "/";
+
+	useEffect(() => {
+		console.log("hello from use Effect login screen");
+		if (userInfo) {
+			navigate(redirect);
+		}
+	}, [userInfo, redirect, navigate]);
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await login({ email, password }).unwrap();
+			dispatch(setCredentials({ ...res }));
+			navigate(redirect);
+		} catch (err) {
+			toast.error(err?.data?.message || err.error);
+		}
+		e.preventDefault();
 		console.log("submit");
 	};
 
@@ -42,13 +73,23 @@ const LoginScreen = () => {
 				<Button
 					type="submit"
 					variant="primary"
-					className="mt-2">
+					className="mt-2"
+					disabled={isLoading}>
 					Sign In
 				</Button>
+				{isLoading && <Loader />}
 			</Form>
 			<Row className="py-3">
 				<Col>
-					New Customer? <Link to="/register">Register</Link>
+					New Customer?{" "}
+					<Link
+						to={
+							redirect
+								? `/register?redirect=${redirect}`
+								: "/register"
+						}>
+						Register
+					</Link>
 				</Col>
 			</Row>
 		</FormContainer>
