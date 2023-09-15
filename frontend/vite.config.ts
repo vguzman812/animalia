fe113ff9f29b474b6f78ec362227a98f60d05590
vincent <100250184+vguzman812@users.noreply.github.com>
2023-09-14 21:18:52 -0400

    Second production attempt. Deleteed vite eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		eslintPlugin({
-			// setup the plugin
-			cache: false,
-			include: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-			exclude: [],
-		}),
-	],
+	plugins: [react()],
 	server: {
 		proxy: {
 			"/api": "http://localhost:8888",
