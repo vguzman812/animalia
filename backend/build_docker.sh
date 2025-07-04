037e3e417c9f4bc0a7f977b3740e7d0073deb8f1
vguzman812 <vguzman812@gmail.com>
2025-07-04 06:18:19 -0400

    fix build_docker

--- backend/build_docker.sh
+++ backend/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DOCKER_TAG=${1:-my-app}
-DOCKER_DEFAULT_PLATFORM=${2:-linux/arm64}
+DOCKER_DEFAULT_PLATFORM=${2:-linux/amd64}
 
 docker build --platform $DOCKER_DEFAULT_PLATFORM -t $DOCKER_TAG .
