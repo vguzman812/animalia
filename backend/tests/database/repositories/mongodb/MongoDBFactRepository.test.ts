fd18ef188e05dffa920ccf4633d2b7e51a2a97f6
vguzman812 <vguzman812@gmail.com>
2025-07-07 19:00:34 -0400

    remove console.log statement

--- backend/tests/database/repositories/mongodb/MongoDBFactRepository.test.ts
+++ backend/tests/database/repositories/mongodb/MongoDBFactRepository.test.ts
@@ -334,7 +334,6 @@ describe("MongoDBFactRepository Search & findAll", () => {
         it("should sort results by creation date desc", async () => {
             /** Test that findAll sorts all facts by creation date in descending order */
             const result = await repository.findAll({});
-            console.log("result :>> ", result);
 
             expect(result.data).toHaveLength(4);
             expect(result.data[0].createdAt.getTime()).toBeGreaterThan(
